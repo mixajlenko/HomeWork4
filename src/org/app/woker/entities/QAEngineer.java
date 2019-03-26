47b4ec5ca73c00f75b2e1bcc8c50b4a04c3ecce0
alitvinov <alexlitvinovpost@gmail.com>
2019-03-26 12:34:45 +0200

    Home Work 4 - base version;

--- src/org/app/woker/entities/QAEngineer.java
+++ src/org/app/woker/entities/QAEngineer.java
@@ -5,30 +5,6 @@ package org.app.woker.entities;
  *
  * @author alitvinov
  */
-public class QAEngineer extends Worker {
+public class QAEngineer {
 
-    private boolean automation;
-
-    public QAEngineer(String workerId, String workerName, boolean automation) {
-        super(workerId, workerName);
-        this.automation = automation;
-    }
-
-    public boolean isAutomation() {
-        return automation;
-    }
-
-    public void setAutomation(boolean automation) {
-        this.automation = automation;
-    }
-
-    @Override
-    public String toString() {
-        return "QAEngeneer{" +
-                "automation=" + automation +
-                ", workerId='" + workerId + '\'' +
-                ", workerName='" + workerName + '\'' +
-                ", workerPosition='" + workerPosition + '\'' +
-                '}';
-    }
 }
